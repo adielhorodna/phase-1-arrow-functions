7d29a06088700fb02cebd91790c2e1c558363780
Adiel Eichenstein <adiel.eich@gmail.com>
2022-12-13 20:39:35 -0500

    commit tree c012d76678e0d3c1773d9384b2ee76c00c3f1741

---
+++ index.js
@@ -0,0 +1,14 @@
+//const divide =() => {
+    //}
+
+
+ const divide =(a,b) => {
+    return a/b
+
+ }
+ const square =parameter =>{
+    return parameter * parameter
+ }
+   const add = (a,b) => {
+    return a+b
+   }
